120a0433cfc699bac4db9296a55d6ce5ce403f13
unknown <demondrake00@gmail.com>
2024-05-03 09:16:07 +0800

    Update TradesHistory.vue
    add and use openTrade api

--- src/components/TradesHistory.vue
+++ src/components/TradesHistory.vue
@@ -39,29 +39,27 @@ const props = defineProps({
     masterAccount: Object
 })
 
-const tradeHistories = ref([])
-const accountId = ref()
+const openTrades = ref([])
+const master_meta_login = ref();
 
 async function fetchData() {
     try {
-        const response = await axios.get(`https://www.myfxbook.com/api/get-open-trades.json?session=4gcHQQj80BSwyYjywWCy3636342&id=${accountId.value}`);
+        const response = await axios.get(`http://127.0.0.1:8000/api/getMasterOpenTrade?meta_login=${master_meta_login.value}`);
         
-        tradeHistories.value = response.data; // Set the metaLogin value upon successful login
-        // console.log('Trade History: ', tradeHistories.value)
+        openTrades.value = response.data.openTrade; // Set the metaLogin value upon successful login
+        // console.log('Open Trade: ', openTrades.value)
     } catch (error) {
         console.error('Error fetching open trade data:', error);
         console.log('Login Status: Failed');    
     }
 }
 
-// Fetch data initially and whenever accountId changes
-// onMounted(fetchData);
 
-// Also watch for changes in tradeHistories
+// Also watch for changes in openTrades
 watch(() => props.masterAccount, (newMasterAccount) => {
-    if (props.masterAccount) {
-        accountId.value = newMasterAccount.id
-        fetchData();
+    if (newMasterAccount) {
+        master_meta_login.value = newMasterAccount.login; // Update master_meta_login with the new value
+        fetchData(); // Fetch data with the updated meta_login value
     }
 });
 
@@ -70,8 +68,8 @@ watch(() => props.masterAccount, (newMasterAccount) => {
 
 // // Calculate duration of close - open date and time 
 // // Function to calculate duration in seconds
-// const calculateDuration = (openTime, closeTime) => {
-//     const openDate = new Date(openTime);
+// const calculateDuration = (time_open, closeTime) => {
+//     const openDate = new Date(time_open);
 //     const closeDate = new Date(closeTime);
 
 //     const durationMilliseconds = closeDate - openDate;
@@ -122,312 +120,64 @@ watch(() => props.masterAccount, (newMasterAccount) => {
                         <th scope="col" class="px-6 py-3">
                             Open Price
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <!-- <th scope="col" class="px-6 py-3">
                             Pips
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Net Profit (USD)
-                        </th>
+                        </th> -->
                         <!-- <th scope="col" class="px-6 py-3">
                             Gain
                         </th> -->
                     </tr>
                 </thead>
-                <tbody v-if="tradeHistories.openTrades && tradeHistories.openTrades.length > 0">
-                    <!-- <tr v-for="tradeHistory in tradeHistories" :key="tradeHistory.id" class="table-content-bckg"> -->
-                        <tr v-for="(tradeHistory, index) in tradeHistories.openTrades.slice(0, 5)" :key="index" :class="{ 'table-content-bckg': index % 2 === 0, 'table-content': index % 2 !== 0 }">
+                <tbody v-if="openTrades && openTrades.length > 0">
+                    <!-- <tr v-for="openTrade in openTrades" :key="openTrade.id" class="table-content-bckg"> -->
+                        <tr v-for="(openTrade, index) in openTrades.slice(0, 5)" :key="index" :class="{ 'table-content-bckg': index % 2 === 0, 'table-content': index % 2 !== 0 }">
                         <td class="px-6 py-4">
                             <div class="table-content-date-time">
                                 <div class="table-content-date">
                                     <!-- 01.01.2024 -->
-                                    <!-- {{ tradeHistory.openTime }} -->
-                                    {{ tradeHistory.openTime.split(' ')[0] }}
+                                    <!-- {{ openTrade.time_open }} -->
+                                    {{ openTrade.time_open.split(' ')[0] }}
                                 </div>
                                 <div class="table-content-time">
                                     <!-- 11:37:48 -->
-                                    {{ tradeHistory.openTime.split(' ')[1] }}
+                                    {{ openTrade.time_open.split(' ')[1] }}
                                 </div>
                             </div>
                         </td>
                         <td class="px-6 py-4">
                             <!-- GBPUSD -->
-                            {{ tradeHistory.symbol }}
+                            {{ openTrade.symbol }}
                         </td>
                         <td class="px-6 py-4">
                             <!-- Sell -->
-                            {{ tradeHistory.action }}
+                            {{ openTrade.trade_type }}
                         </td>
                         <td class="px-6 py-4">
                             <!-- 500.00 -->
-                            {{ tradeHistory.sizing.value }}
+                            {{ openTrade.volume }}
                         </td>
                         <td class="px-6 py-4">
                             <!-- 1.29506 -->
-                            {{ tradeHistory.openPrice }}
+                            {{ openTrade.price_open }}
                         </td>
-                        <td class="px-6 py-4">
-                            <span :style="{ color: tradeHistory.pips > 0 ? '#06F7A1' : (tradeHistory.pips < 0 ? '#FF483D' : 'inherit') }">
-                                {{ tradeHistory.pips }}
+                        <!-- <td class="px-6 py-4">
+                            <span :style="{ color: openTrade.pips > 0 ? '#06F7A1' : (openTrade.pips < 0 ? '#FF483D' : 'inherit') }">
+                                {{ openTrade.pips }}
                             </span>
-                        </td>
-                        <td class="px-6 py-4">
+                        </td> -->
+                        <!-- <td class="px-6 py-4"> -->
                             <!-- -25,000.00 -->
-                            <!-- {{ tradeHistory.profit }} -->
-                            <span :style="{ color: tradeHistory.profit > 0 ? '#06F7A1' : (tradeHistory.profit < 0 ? '#FF483D' : 'inherit') }">
-                                {{ tradeHistory.profit }}
+                            <!-- {{ openTrade.profit }} -->
+                            <!-- <span :style="{ color: openTrade.profit > 0 ? '#06F7A1' : (openTrade.profit < 0 ? '#FF483D' : 'inherit') }">
+                                {{ openTrade.profit }}
                             </span>
-                        </td>
+                        </td> -->
                         <!-- <td class="px-6 py-4">
                         </td> -->
                     </tr>
-                    <!-- <tr class="table-content">
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:59:16
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:58:54
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            GBPUSD
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            Sell
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            500.00
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29506
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29556
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            -5.0
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            14,372.32
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            38s
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            13.23%
-                            {{ loginStatus }}
-                        </td>
-                    </tr>
-                    <tr class="table-content-bckg">
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:50:12
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:52:00
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            GBPUSD
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            Sell
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            500.00
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29506
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29556
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            -5.0
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            75,373.83
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            108s
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            67.27%
-                            {{ loginStatus }}
-                        </td>
-                    </tr>
-                    <tr class="table-content">
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:38:03
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:38:56
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            GBPUSD
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            Sell
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            500.00
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29506
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29556
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            -5.0
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            -2,378.99
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            53s
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            -12.36%
-                            {{ loginStatus }}
-                        </td>
-                    </tr>
-                    <tr class="table-content-bckg">
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:00:38
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            <div class="table-content-date-time">
-                                <div class="table-content-date">
-                                    01.01.2024
-                                    {{ loginStatus }}
-                                </div>
-                                <div class="table-content-time">
-                                    10:00:52
-                                    {{ loginStatus }}
-                                </div>
-                            </div>
-                        </td>
-                        <td class="px-6 py-4">
-                            GBPUSD
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            Sell
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            500.00
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29506
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            1.29556
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            -5.0
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            93,383.37
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            14s
-                            {{ loginStatus }}
-                        </td>
-                        <td class="px-6 py-4">
-                            26.91%
-                            {{ loginStatus }}
-                        </td>
-                    </tr> -->
                 </tbody>
             </table>
         </div>
